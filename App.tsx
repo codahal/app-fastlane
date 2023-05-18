f4bc614df4696be7cae79128b6ad4744f5849472
jagadish <pandu516jagu@gmail.com>
2023-05-18 12:00:37 +0530

    updated

--- App.tsx
+++ App.tsx
@@ -77,8 +77,7 @@ function App(): JSX.Element {
             backgroundColor: isDarkMode ? Colors.black : Colors.white,
           }}>
           <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
+            Edit <Text style={styles.highlight}>App.tsx</Text> harsha
           </Section>
           <Section title="See Your Changes">
             <ReloadInstructions />
@@ -87,7 +86,7 @@ function App(): JSX.Element {
             <DebugInstructions />
           </Section>
           <Section title="Learn More">
-            Read the docs to discover what to do next:
+            hello
           </Section>
           <LearnMoreLinks />
         </View>
